a35ad79f8aae5572404d3c9c6623db61c1c66572
patrickspafford <patrickspafford1@gmail.com>
2021-05-22 18:28:14 -0500

    Rebuild with proper conditions

--- dist/index.js
+++ dist/index.js
@@ -14,7 +14,7 @@ const useFirestoreListener = (config) => {
         let cr = !((_a = config.options) === null || _a === void 0 ? void 0 : _a.isCollectionGroup)
             ? firestore.collection(config.collection)
             : firestore.collectionGroup(config.collection);
-        if (!((_b = config.options) === null || _b === void 0 ? void 0 : _b.conditions)) {
+        if ((_b = config.options) === null || _b === void 0 ? void 0 : _b.conditions) {
             (_c = config.options) === null || _c === void 0 ? void 0 : _c.conditions.forEach((condition) => {
                 const { field, operator, value } = condition;
                 cr = cr.where(field, operator, value);
